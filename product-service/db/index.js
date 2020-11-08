6db5569e8bc31790cfd4fc774834f930d1680ef4
Denis Shiriaev <denis.shiriaev@novartis.com>
2020-11-08 23:50:58 +0300

    feat: Add product to DB, update swagger

--- product-service/db/index.js
+++ product-service/db/index.js
@@ -1,4 +1,5 @@
 import DbError from '../common/DbError';
+import ValidationError from '../common/ValidationError';
 import { Client } from 'pg';
 
 const {
@@ -21,7 +22,7 @@ const dbOptions = {
   connectionTimeoutMillis: 5000
 };
 
-export const getProducts = async event => {
+export const getProducts = async () => {
   const client = new Client(dbOptions);
 
   try {
@@ -77,6 +78,67 @@ export const getProductById = async (id) => {
 
     return items;
   } catch (err) {
+    console.error(err);
+    throw new DbError(err.message);
+  } finally {
+    client.end();
+  }
+}
+
+export const addProduct = async (data) => {
+  const client = new Client(dbOptions);
+
+  let product;
+
+  try {
+    product = JSON.parse(data);
+  } catch (err) {
+    throw new ValidationError(`Can't parser product data {${data}}`);
+  }
+
+  const {
+    title,
+    description,
+    price,
+    image,
+    stock
+  } = product;
+
+  try {
+    await client.connect();
+  } catch (err) {
+    console.error(err);
+    throw new DbError(err.message);
+  }
+
+  try {
+    await client.query('BEGIN');
+    const insertProductQuery = 'INSERT INTO products (title, price, description, image) VALUES ($1, $2, $3, $4) RETURNING id';
+
+    console.log('SQL Insert Product', insertProductQuery, [title, price, description, image]);
+
+    const res = await client.query(insertProductQuery, [title, price, description, image]);
+
+    console.log('Res', res);
+
+    const [ entry ] = res.rows;
+    const insertStockQuery = 'INSERT INTO stocks (product_id, count) VALUES ($1, $2)';
+
+    console.log('SQL Insert Stock', insertStockQuery, [entry.id, stock]);
+
+    await client.query(insertStockQuery, [entry.id, stock]);
+    await client.query('COMMIT')
+
+    return {
+      ...entry,
+      title,
+      description,
+      price,
+      image,
+      stock,
+    };
+  } catch (err) {
+    await client.query('ROLLBACK');
     console.error(err);
     throw new DbError(err.message);
   } finally {
--- product-service/handler.js
+++ product-service/handler.js
@@ -1,7 +1,9 @@
 import * as db from './db';
+import addProductHandler from './handlers/addProduct';
 import getProductsListHandler from './handlers/getProductsList';
 import getProductByIdHandler  from './handlers/getProductById';
 import responseWrapper from './common/responseWrapper';
 
+export const addProduct = responseWrapper(addProductHandler(db));
 export const getProductsList = responseWrapper(getProductsListHandler(db));
 export const getProductById = responseWrapper(getProductByIdHandler(db));
