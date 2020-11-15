cc09a93851ee6c8a84b1cc11e2a3bf7d6451d464
Denis Shiriaev <denis.shiriaev@novartis.com>
2020-11-15 22:19:50 +0300

    fix: Change lambda function names

--- import-service/handler.js
+++ import-service/handler.js
@@ -2,5 +2,5 @@ import importFileParserHandler from './handlers/importFileParser';
 import importProductsFileHandler from './handlers/importProductsFile';
 import responseWrapper from './common/responseWrapper';
 
-export const addProduct = responseWrapper(importFileParserHandler);
-export const getProductsList = responseWrapper(importProductsFileHandler);
+export const importFileParser = responseWrapper(importFileParserHandler);
+export const importProductsFile = responseWrapper(importProductsFileHandler);
